340a5182e779e2a3e2592def77323c6aef82dc98
野村優羽 <@MSI>
2023-10-23 11:14:03 +0900

    中断

--- main.cpp
+++ main.cpp
@@ -1,5 +1,42 @@
-int main() {
+#include <stdio.h>
 
-	return 0;
+class Finger {
 
+public: // メンバ関数
+
+	Finger() = default;
+	~Finger() = default;
+
+	virtual void NameDisplay() = 0;
+
+protected: //メンバ変数
+
+	const char* name_;
+
+};
+
+class Thumb : public Finger {
+
+public: // メンバ関数
+
+	Thumb();
+	~Thumb();
+
+	virtual void NameDisplay();
+
+};
+
+Thumb::Thumb()
+{
+
+	name_ = "親指\n";
+
+}
+
+Thumb::~Thumb()
+{
+}
+
+void Thumb::NameDisplay()
+{
 }
