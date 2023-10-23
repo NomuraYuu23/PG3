56ccda6c6c1c3fe50aa89cc00e3ea622cbfb4f2e
野村優羽 <@MSI>
2023-10-23 11:47:22 +0900

    指クラス実装

--- main.cpp
+++ main.cpp
@@ -71,3 +71,60 @@ void MiddleFinger::NameDisplay()
 	printf("中指\n");
 }
 
+/// <summary>
+/// 薬指クラス
+/// </summary>
+class RingFinger : public Finger {
+
+public: // メンバ関数
+
+	RingFinger() = default;
+	~RingFinger() = default;
+
+	virtual void NameDisplay();
+
+};
+
+void RingFinger::NameDisplay()
+{
+	printf("薬指\n");
+}
+
+/// <summary>
+/// 小指クラス
+/// </summary>
+class LittleFinger : public Finger {
+
+public: // メンバ関数
+
+	LittleFinger() = default;
+	~LittleFinger() = default;
+
+	virtual void NameDisplay();
+
+};
+
+void LittleFinger::NameDisplay()
+{
+	printf("小指\n");
+}
+
+/// <summary>
+/// メイン関数
+/// </summary>
+/// <returns></returns>
+int main() {
+
+	Finger* fingers[5];
+	fingers[0] = new Thumb();
+	fingers[1] = new IndexFinger();
+	fingers[2] = new MiddleFinger();
+	fingers[3] = new RingFinger();
+	fingers[4] = new LittleFinger();
+
+	for (size_t i = 0; i < 5; i++) {
+		fingers[i]->NameDisplay();
+		delete fingers[i];
+	}
+
+}
